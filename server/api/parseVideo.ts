44d23025b61253967e0136cba4f9a48bc3b0e830
ruiange <bombingpig@outlook.com>
2024-10-25 10:08:18 +0800

    feat(server): 添加视频解析接口- 在 server/api 目录下新增 parseVideo.ts 文件 - 实现了一个处理视频解析的 API接口 - 使用 axios 发送请求到第三方 API 进行视频数据解析 - 返回解析后的视频数据或错误信息

---
+++ server/api/parseVideo.ts
@@ -0,0 +1,31 @@
+import {defineEventHandler, readBody} from 'h3'
+import axios from "axios";
+
+/**
+ * 解析视频链接
+ */
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event)
+    if (!body.url) {
+        return {
+            code: 400,
+            msg: '复制🔗不能为空'
+        }
+    }
+    try{
+        const {data} = await axios({
+            method: 'get',
+            url: 'http://101.126.92.189:8090/api/hybrid/video_data',
+            params: {
+                url: body.url,
+                minimal: true
+            }
+        })
+        return data
+    }catch (e:any){
+        return {
+            code: 400,
+            msg: '解析失败'+ e.message
+        }
+    }
+})
