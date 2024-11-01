7d59ddf709e85b9de9b109ba3b79f289861fb3f4
ruiange <bombingpig@outlook.com>
2024-11-01 17:38:03 +0800

    feat(server): 添加删除吧内禁言接口
    - 实现了一个 POST 请求的处理器，用于删除吧内禁言记录
    - 集成了 axios 库来发送 DELETE 请求到第三方 API
    - 添加了基本的输入验证和错误处理

---
+++ server/api/tieba/ban/delete.ts
@@ -0,0 +1,39 @@
+import {defineEventHandler, readBody} from 'h3'
+import axios from "axios";
+import {id} from "postcss-selector-parser";
+
+export default defineEventHandler(async (event) => {
+    if (event._method !== 'POST') {
+        return new Response(null, {
+            status: 302,
+            headers: {
+                Location: '/',
+            },
+        })
+    }
+    const body = await readBody(event);
+
+    if (!body.id) {
+        return {
+            code: 400,
+            message: 'id is required'
+        }
+    }
+    const id = body.id;
+
+    try {
+        const {data} = await axios({
+            method: 'delete',
+            url: 'https://tiebaapi.ruiange.work/ban',
+            data: {
+                id: id
+            }
+        });
+        return data;
+    } catch (e) {
+        return {
+            code: 500,
+            message: e.message
+        }
+    }
+});
